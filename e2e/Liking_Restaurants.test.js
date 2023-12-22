df27a2523979becd20599fcb0ce436f1dc2cf058
Yasir Amri <yasiramrr@gmail.com>
2023-12-22 13:19:47 +0700

    update test

--- e2e/Liking_Restaurants.test.js
+++ e2e/Liking_Restaurants.test.js
@@ -1,3 +1,5 @@
+const { async } = require("regenerator-runtime");
+
 Feature('Liking Restaurants');
 
 Before(({ I }) => {
@@ -9,8 +11,18 @@ Scenario('showing empty favorite restaurants', ({ I }) => {
   I.see('Tidak ada restaurant untuk ditampilkan', '.restaurant-item__not__found');
 });
 
-Scenario('liking one restaurant', ({ I }) => {
+Scenario('liking one restaurant', async ({ I }) => {
   I.see('Tidak ada restaurant untuk ditampilkan', '.restaurant-item__not__found');
   I.amOnPage('/');
-  pause();
+
+  I.waitForElement('.restaurant__name a');
+  const firstRestaurant = locate('.restaurant__name a').first();
+  const firstRestaurantName = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
+
+  I.waitForElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.restaurant-item');
 });
