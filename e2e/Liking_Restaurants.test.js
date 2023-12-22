ab8fb18dc223fccdce83aa15338c1fb0b947cbab
Yasir Amri <yasiramrr@gmail.com>
2023-12-22 14:34:49 +0700

    update test

--- e2e/Liking_Restaurants.test.js
+++ e2e/Liking_Restaurants.test.js
@@ -1,4 +1,5 @@
-const { async } = require("regenerator-runtime");
+const { async } = require('regenerator-runtime');
+const assert = require('assert');
 
 Feature('Liking Restaurants');
 
@@ -25,4 +26,49 @@ Scenario('liking one restaurant', async ({ I }) => {
 
   I.amOnPage('/#/favorite');
   I.seeElement('.restaurant-item');
+  const favoritesRestaurantName = await I.grabTextFrom('.restaurant__name');
+
+  assert.strictEqual(firstRestaurantName, favoritesRestaurantName);
+});
+
+Scenario('searching restaurants', async ({ I }) => {
+  I.see('Tidak ada restaurant untuk ditampilkan', '.restaurant-item__not__found');
+  I.amOnPage('/');
+
+  I.waitForElement('.restaurant__name a');
+
+  const names = [];
+  for (let i = 1; i <= 3; i++) {
+    I.click(locate('.restaurant__name a').at(i));
+
+    I.waitForElement('#likeButton');
+    I.click('#likeButton');
+
+    // eslint-disable-next-line no-await-in-loop
+    names.push(await I.grabTextFrom('.restaurant__name'));
+
+    I.amOnPage('/');
+  }
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('#query');
+
+  const visibleFavoritesRestaurants = await I.grabNumberOfVisibleElements('.restaurant-item');
+  assert.strictEqual(names.length, visibleFavoritesRestaurants);
+
+  const searchQuery = names[1].substring(1, 3);
+
+  I.fillField('#query', searchQuery);
+  I.pressKey('Enter');
+
+  const matchingRestaurants = names.filter((name) => name.indexOf(searchQuery) !== -1);
+  const visibleSearchedFavoritesRestaurants = await I.grabNumberOfVisibleElements('.restaurant-item');
+
+  assert.strictEqual(matchingRestaurants.length, visibleSearchedFavoritesRestaurants);
+
+  for (let i = 0; i < matchingRestaurants.length; i++) {
+    const visibleName = await I.grabTextFrom(locate('.restaurant__name').at(i +1));
+
+    assert.strictEqual(matchingRestaurants[i], visibleName);
+  }
 });
